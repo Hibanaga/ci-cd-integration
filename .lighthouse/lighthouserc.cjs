b08525d68207bf040371022dc7c61b5a5b11ddc1
Hibanaga <vladyslav.tykhoniuk.works@gmail.com>
2025-08-19 08:43:54 +0200

    try 2.6

--- .lighthouse/lighthouserc.cjs
+++ .lighthouse/lighthouserc.cjs
@@ -15,33 +15,33 @@ if (FORM_FACTOR === 'desktop') {
 module.exports = {
   ci: {
     collect: {
-      numberOfRuns: 1,
+      numberOfRuns: 3,
       settings,
       chromeFlags: '--no-sandbox --disable-gpu',
     },
     assert: {
       assertions: {
-        'categories:performance': ['error', { minScore: 0.50 }],
-        'categories:accessibility': ['warn', { minScore: 0.50 }],
-        'categories:best-practices': ['warn', { minScore: 0.50 }],
-        'categories:seo': ['warn', { minScore: 0.50 }],
+        "categories:performance": ["error", { "minScore": 0.50 }],
+        "categories:accessibility": ["error", { "minScore": 0.50 }],
+        "categories:best-practices": ["error", { "minScore": 0.50 }],
+        "categories:seo": ["error", { "minScore": 0.50 }],
 
-        'first-contentful-paint': ['warn', { maxNumericValue: 8000, aggregationMethod: 'median' }],
-        'largest-contentful-paint': ['warn', { maxNumericValue: 4000, aggregationMethod: 'median' }],
-        'total-blocking-time': ['warn', { maxNumericValue: 900, aggregationMethod: 'median' }],
-        'cumulative-layout-shift': ['warn', { maxNumericValue: 0.2, aggregationMethod: 'median' }],
-        'speed-index': ['warn', { maxNumericValue: 8000, aggregationMethod: 'median' }],
+        "first-contentful-paint": ["warn", { "maxNumericValue": 8000, "aggregationMethod": "median" }],
+        "largest-contentful-paint": ["warn", { "maxNumericValue": 4000, "aggregationMethod": "median" }],
+        "total-blocking-time": ["warn", { "maxNumericValue": 900, "aggregationMethod": "median" }],
+        "cumulative-layout-shift": ["warn", { "maxNumericValue": 0.2, "aggregationMethod": "median" }],
+        "speed-index": ["warn", { "maxNumericValue": 8000, "aggregationMethod": "median" }],
 
-        'unused-javascript': 'warn',
-        'unused-css-rules': 'warn',
-        'legacy-javascript': 'warn',
-        'render-blocking-resources': 'warn',
-        'offscreen-images': 'warn',
-        'uses-responsive-images': 'warn',
+        "unused-javascript": "error",
+        "unused-css-rules": "error",
+        "legacy-javascript": "error",
+        "render-blocking-resources": "error",
+        "offscreen-images": "error",
+        "uses-responsive-images": "error",
 
-        'resource-summary': 'error',
-        'uses-long-cache-ttl': 'warn',
-        'no-document-write': 'error',
+        "resource-summary": "error",
+        "uses-long-cache-ttl": "error",
+        "no-document-write": "error"
       },
       budgetsFile: './.lighthouse/budget.json',
     },
